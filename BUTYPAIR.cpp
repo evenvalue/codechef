5d6d72b648b5fef5834e6a554565ef6034cecbfc
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-08-01 12:29:14 +0530

    Add math problem.
    Beautiful Pairs: https://www.codechef.com/problems/BUTYPAIR

---
+++ BUTYPAIR.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define debug(x) cerr << #x << ": " << x << '\n'
+using int64 = int64_t;
+
+const int64 kInfL = 1e15 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> cnt(1e6);
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    cnt[x - 1]++;
+  }
+  int64 ans = 0;
+  for (int i = 0; i < 1e6; i++) {
+    ans += (int64)cnt[i] * (n - cnt[i]);
+  }
+  cout << ans << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
