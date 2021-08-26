9989e613f85d8526895044b89bc28e1143fa834f
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-08-26 09:29:03 +0530

    Add bits problem.
    Weird Ordering: https://www.codechef.com/ZCOPRAC/problems/ZCO20001

---
+++ ZCO20001.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define debug(x) cerr << #x << ": " << x << '\n'
+using int64 = int64_t;
+
+const int64 kInfL = 1e15 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+ifstream fin("input.txt");
+ofstream fout("output.txt");
+
+void solve() {
+  int64 p, idx;
+  cin >> p >> idx;
+  int64 ans = 0;
+  for (int64 mid = (1LL << (p - 1)), i = 0; idx != 0; mid /= 2, i++) {
+    if (idx >= mid) {
+      ans += (1LL << i);
+      idx -= mid;
+    }
+  }
+  cout << ans << '\n';
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
